80937e5b586529943eedc34284946bae36a15e98
EMZEDI <77243080+EMZEDI@users.noreply.github.com>
2022-01-12 10:31:26 -0500

    Last submission. edit the readme file !

--- employee/models.py
+++ employee/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(default='Quantity = 1, Date added:' + str(datetime.date.today()))
+    quantity = models.IntegerField(default=1)
     price = models.FloatField(max_length=20, default=1.99)
 
     class Meta:
