66b108b364f97669691f7a456d10910537d60941
EMZEDI <77243080+EMZEDI@users.noreply.github.com>
2022-01-12 08:12:11 -0500

    It works - change models.py fields.

--- employee/models.py
+++ employee/models.py
@@ -1,9 +1,11 @@
+import datetime
+
 from django.db import models
 
 
 class Product(models.Model):
     name = models.CharField(max_length=100)
-    description = models.TextField(default='N/A')
+    description = models.TextField(default='Quantity = 1, Date added:' + str(datetime.date.today()))
     price = models.FloatField(max_length=20, default=1.99)
 
     class Meta:
